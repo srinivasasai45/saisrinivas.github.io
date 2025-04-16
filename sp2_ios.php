18fb93f54066ea5dd48e40b9a9529e51ebeeb8e0
Sai Srinivas <146947289+srinivasasai45@users.noreply.github.com>
2025-04-16 22:25:30 +0530

    sp2_ios.php

---
+++ sp2_ios.php
@@ -0,0 +1,18 @@
+const playerInstance = jwplayer("jwplayerDiv").setup({
+    skin: {
+       name: "Denver",
+       active: "RED",
+       inactive: "white",
+       background: "black"
+    },
+    autostart: true,
+    preload: "none",
+    repeat: true,
+    volume: 100,
+    mute: false,
+    stretching: "exactfit",
+    width: "100%", 
+    cast: {},
+    type: "hls",
+    file: "t6ZnotAtftd0nhNUXb834geIM1ph55hf-cc%3din-de%3d1-pl%3dandroid-ap%3dtg-psplaylink-ut%3dpaid-ttl%3d86400-type%3dfree-~hmac=3e712075791be9a28e4cafa39ed606530efa68c795ef15a823bee1764ac9b2c8&user-agent=Hotstar;in.startv.hotstar/ TG - @psplaylink"
+});
